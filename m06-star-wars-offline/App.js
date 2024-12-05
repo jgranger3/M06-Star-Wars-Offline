767d4ddccc2dda027b0dc3efa4f3a14a5df2ab40
jgranger3 <jgranger3@ivytech.edu>
2024-12-05 14:09:38 -0500

    offline

---
+++ m06-star-wars-offline/App.js
@@ -0,0 +1,52 @@
+import  React, {useState, useEffect} from 'react';
+import {
+  Text,
+  SafeAreaView,
+  Button,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import PlanetsScreen from './PlanetsScreen';
+import FilmsScreen from './FilmsScreen';
+import SpaceshipsScreen from './SpaceshipsScreen';
+
+const Stack = createStackNavigator();
+const HomeScreen = ({ navigation }) => (
+  <SafeAreaView>
+    <Text> Welcome to the Star Wars Universe!!!! </Text>
+    <Button title="Planets" onPress={() => navigation.navigate('Planets')} />
+    <Button title="Films" onPress={() => navigation.navigate('Films')} />
+    <Button
+      title="Spaceships"
+      onPress={() => navigation.navigate('Spaceships')}
+    />
+  </SafeAreaView>
+);
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Planets" component={PlanetsScreen} />
+        <Stack.Screen name="Films" component={FilmsScreen} />
+        <Stack.Screen name="Spaceships" component={SpaceshipsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+
+  const [isConnected, setIsConnected] = useState(true);
+  
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected);
+    });
+  });
+};
+
+
+
+export default App;
